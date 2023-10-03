0548ca0b709955a94556d9e160b638ae299ec816
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2023-10-03 16:10:11 +0500

    Update task11.py

--- mod2/task11.py
+++ mod2/task11.py
@@ -1,2 +1,13 @@
-a = input().split()
-print(len(a) != len(set(a)))
+def same_numbers(str):
+    temp = ""
+    for i in range(len(str)):
+        if (not str[i].isdigit()):
+            continue;
+        if (str[i] in temp):
+            return True
+        temp += str[i];
+    return False
+    
+
+str = input()
+print(same_numbers(str))
