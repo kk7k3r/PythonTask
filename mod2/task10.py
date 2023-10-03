237e6ff7ba0798f52ce237f8ce65f5076c5f658f
kk7k3r <119610131+kk7k3r@users.noreply.github.com>
2023-10-03 16:09:56 +0500

    Update task10.py

--- mod2/task10.py
+++ mod2/task10.py
@@ -1,6 +1,11 @@
-strs = input().split()
+str = input()
 res = ""
-for i in range(len(strs)):
-    res+= strs[i][-1]
+previous_symb = ""
+for i in range(len(str)):
+    if (str[i] == " "):
+        res += previous_symb
+    if (i == len(str) - 1):
+        res += str[i]
+    previous_symb = str[i]
 print(res)
     
